0e152d0324565552310dca6b32431d2e7c91b5e0
NAM NGUYEN <Nam.Nguyen@ibm.com>
2021-05-10 22:44:51 -0400

    Complete Section 2

---
+++ src/app/auth/reducers/index.ts
@@ -0,0 +1,34 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createReducer,
+  createSelector,
+  MetaReducer,
+  on,
+} from '@ngrx/store';
+import { AuthActions } from '../action-types';
+import { User } from '../model/user.model';
+
+export const authFeatureKey = 'auth';
+
+export interface AuthState {
+  user: User;
+}
+
+export const initialAuthState: AuthState = {
+  user: undefined,
+};
+
+export const authReducer = createReducer(
+  initialAuthState,
+  on(AuthActions.login, (state, action) => {
+    console.log('Calling log in reducer');
+
+    debugger;
+
+    return {
+      user: action.user,
+    };
+  })
+);
